7fd64c9868091f8c91507e52ffc4d36cea4211ed
AdityaNik <aadityanikam2004@gmail.com>
2025-02-08 12:33:57 +0530

    abcc

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import sgMail from "@sendgrid/mail";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 dotenv.config();
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
@@ -52,6 +55,63 @@ app.post("/verify-otp", (req, res) => {
   }
 
   res.json({ success: false, message: "Invalid OTP" });
+});
+
+import multer from "multer";
+const upload = multer({ dest: "uploads/" });
+
+app.post("/create-campaign", upload.single("imageUrl"), async (req, res, next) => {
+  try {
+    const { title, description, goal, milestones, imageUrl } = req.body.data1;
+    const campaign = await prisma.campaign.create({
+      data: {
+        title,
+        description,
+        imageUrl,
+        raised: "0",
+        goal,
+        daysLeft: 30,
+      },
+    });
+
+    if (milestones.length > 0) {
+      await prisma.milestone.createMany({
+        data: milestones.map((milestone) => ({
+          title: milestone.title,
+          amount: milestone.amount,
+          status: milestone.status || "pending", // Default status if missing
+          campaignId: campaign.id,
+        })),
+      });
+    }
+
+    res.json({ success: true, message: "Campaign created successfully with ID: " + campaign.id });
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ success: false, message: "Internal server error" });
+  }
+});
+
+
+app.get('/campaigns', async (req, res) => {
+  const result = await prisma.campaign.findMany({});
+  res.json(result);
+});
+
+app.get('/milestones', async (req, res) => {
+  const result = await prisma.milestone.findMany();
+  res.json(result);
+});
+
+app.get('/campaigns/:id', async (req, res) => {
+  const { id } = req.params;
+  console.log(id)
+  const result = await prisma.campaign.findUnique({
+    where: {
+      id,
+    },
+  });
+  res.json(result);
 });
 
 
